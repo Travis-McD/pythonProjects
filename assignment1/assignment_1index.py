0eb7570eb230f6fbbe9fd56ffc7e3a6b5069f83e
Travis McDaniel <mcdt777@gmail.com>
2021-09-19 22:04:57 -0400

    add assignment1 project

---
+++ assignment1/assignment_1index.py
@@ -0,0 +1,27 @@
+# This program prints a simple statement
+# Name: Travis McDaniel
+# Date: 10/08/2020
+
+# print('Travis Mcdaniel')
+
+# write program that prints numbers in sequence 0 1000 every 10 times
+
+# for x in range(0, 1001, 10):
+#    print(x)
+
+# total = 0.0
+# counter = 0
+# for counter in range(1, 6):
+#    number = float(input('Enter a number: '))
+#    total = total + number
+#    print('The total is: ', total)
+#    print('The total number of values entered is: ', counter)
+
+#for row in range(8):
+  #  for col in range(6):
+  #      print('*', end='')
+  #  print()
+
+count = 10
+while count < 1:
+    print('hello tcc')
