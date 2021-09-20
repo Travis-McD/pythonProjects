8a1d8b8a091386a396537971cb429eacb5f88d1a
Travis McDaniel <mcdt777@gmail.com>
2021-09-19 21:54:05 -0400

    add calculateAge project

---
+++ calculateAge/ageIndex.py
@@ -0,0 +1,19 @@
+# This is a program to calculate age
+# programmed by Travis McDaniel
+# 10/14/2020
+
+# input values (step 1)
+
+yearBorn = input("What year were you born? ")
+yearBorn = int(yearBorn)
+
+thisYear = 2020
+thisYear = int(thisYear)
+
+# calculate age (step 2)
+yourAge = thisYear - yearBorn
+yourAge = int(yourAge)
+
+# Output results
+print("You are", yourAge, "year(s) old.")
+
